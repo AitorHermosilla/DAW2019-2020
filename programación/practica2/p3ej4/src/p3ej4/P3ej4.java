121d815bef0bf4ed8c3013e9d470ddf4dbd5d923
Wallarrap <aitor.hermosilla@ikasle.egibide.org>
2019-10-20 21:49:43 +0200

    dom, 20 de oct
    Archivos atrasados

--- programación/practica2/p3ej4/src/p3ej4/P3ej4.java
+++ programación/practica2/p3ej4/src/p3ej4/P3ej4.java
@@ -10,17 +10,59 @@ Al final, cuando nos tecleen fin, mostraremos la nota media del conjunto de alum
  */
 package p3ej4;
 
-/**
- *
- * @author root
- */
+import javax.swing.JOptionPane;
+
 public class P3ej4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nombre = "";
+        double nota = 0;
+        String mensaje = "";
+        double media = 0;
+        
+        mensaje = IntroducirDatos(nombre, nota, mensaje, media);
+        
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+    public static String IntroducirDatos(String nombre, double nota, String mensaje, double media){
+        boolean error = false;
+        int confirmacion = 0;
+        int cont = 0;
+        String notas = "";
+        do{
+            try{
+                do{
+             
+                    nombre = JOptionPane.showInputDialog("Introduzca el nombre del/la alumn@: ");
+                    
+                    for (int x = 0; x<6;x++){
+                        nota = Double.parseDouble(JOptionPane.showInputDialog("Introduce la"+x+"º"+" nota del/la alumn@"));
+                        notas = notas + "\n nota de la "+x+"ª "+"asignatura = "+nota;
+                    }
+                    nota = Double.parseDouble(JOptionPane.showInputDialog("Introduce la nota del/la alumn@"));
+                
+                    mensaje = mensaje + "\n Nombre: " + nombre + " Notas: " + notas;
+                    
+                    cont ++;
+                    media = media + nota;
+                    
+                    confirmacion = JOptionPane.showConfirmDialog(null, "¿Quieres continuar?");
+                    
+                    error = false;
+                }while(confirmacion == 0);
+                
+                media = media/cont;
+                mensaje = mensaje + "\n \n \n La nota media es: " + media;
+                
+                
+            }catch(NumberFormatException e){
+               error = true;
+               
+               JOptionPane.showMessageDialog(null, "He detectado el siguiente error" + "\n\n\n\n"+e+"\n\n\nHas introducido letras en un valor numerico");
+            }
+            
+        }while(error);
+        return mensaje;
     }
     
 }
